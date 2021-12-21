ae36e9c0f130388c9da208c63d37482b033f0105
Volodymyr Hladkyi <volodymyr.hladkyi@symphony-solutions.com>
2021-12-21 13:32:37 +0200

    added search by criteria using mongo template

--- src/main/java/com/example/mongoapp/MongoAppApplication.java
+++ src/main/java/com/example/mongoapp/MongoAppApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,21 +20,37 @@ public class MongoAppApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
         return args -> {
             Address address = new Address("USA",
                     "New York",
                     "NY123");
+
+            String email = "john@doe.com";
             Student student = new Student("John",
                     "Doe",
-                    "john@doe.com",
+                    email,
                     Gender.MALE,
                     address,
                     List.of("Computer Science", "Maths"),
                     BigDecimal.TEN,
                     LocalDateTime.now());
 
-            repository.insert(student);
+            Query query = new Query();
+            query.addCriteria(Criteria.where("email").is(email));
+
+            List<Student> students = mongoTemplate.find(query, Student.class);
+
+            if (students.size() > 1) {
+                throw new IllegalStateException("Found many students with email " + email);
+            }
+
+            if (students.isEmpty()) {
+                System.out.println("Inserting student " + student);
+                repository.insert(student);
+            } else {
+                System.out.println(student + " already exists");
+            }
         };
     }
 }
